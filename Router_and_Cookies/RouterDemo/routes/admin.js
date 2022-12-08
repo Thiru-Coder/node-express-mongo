edecdf540a15441f6832347173c4f5128fd4c4be
Thiru-Coder <thiruvalluvan1999@gmail.com>
2022-12-08 21:44:53 +0530

    Router, Cookies, Session and Flash

---
+++ Router_and_Cookies/RouterDemo/routes/admin.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+// This Middleware only applies to the admin route
+router.use((req, res, next) => {
+  if (req.query.isAdmin) {
+    return next();
+  }
+  return res.send("SORRY NOT AN ADMIN!");
+});
+
+router.get("/topsecret", (req, res) => {
+  res.send("THIS IS TOP SECRET");
+});
+router.get("/deleteeverything", (req, res) => {
+  res.send("OK DELETED IT ALL!");
+});
+
+module.exports = router;
